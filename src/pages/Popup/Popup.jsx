7fcd3935ef75e2b798ee1ae701d040afe0eb1952
gyf <774663734@qq.com>
2024-05-24 00:52:12 +0800

    导航菜单

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
-import './Popup.css';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Flex, Segmented } from 'antd';
+import './Popup.scss';
 
 const Popup = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <Flex gap="small" align="flex-start" vertical>
+        <Segmented
+          onChange={(e) => {
+            console.log(e);
+            chrome.tabs.create({
+              url: chrome.runtime.getURL('Newtab.html'),
+            });
+          }}
+          options={[
+            {
+              label: (
+                <div style={{ padding: 4 }}>
+                  <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
+                  <div>查看JSON</div>
+                </div>
+              ),
+              value: 'user1',
+            },
+            {
+              label: (
+                <div style={{ padding: 4 }}>
+                  <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+                  <div>代码对比</div>
+                </div>
+              ),
+              value: 'user2',
+            },
+            {
+              label: (
+                <div style={{ padding: 4 }}>
+                  <Avatar
+                    style={{ backgroundColor: '#87d068' }}
+                    icon={<UserOutlined />}
+                  />
+                  <div>常用工具</div>
+                </div>
+              ),
+              value: 'user3',
+            },
+          ]}
+        />
+      </Flex>
     </div>
   );
 };
